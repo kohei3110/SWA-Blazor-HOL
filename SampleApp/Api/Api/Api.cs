b4ad7d4bffa956f606b8240a0d113beeeb1a491f
kohei3110 <kk.31108424@gmail.com>
2023-08-09 10:51:02 +0900

    update api to return mock values

---
+++ SampleApp/Api/Api/Api.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.NetworkInformation;
+
+using Common;
+
+namespace Api
+{
+    public static class Api
+    {
+        [FunctionName("Api")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            Product product = new Product
+            {
+                ID = 111,
+                Name = "Product 111",
+                Description = "製品の説明",
+                Quantity = 1
+            };
+            return new OkObjectResult(product);
+
+        }
+    }
+}
+
