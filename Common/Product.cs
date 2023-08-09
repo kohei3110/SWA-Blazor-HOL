0d2a2c69bf9c80c1ff8f6498d9fc00e3517cf75e
kohei3110 <kk.31108424@gmail.com>
2023-08-09 10:50:23 +0900

    create common data model

---
+++ Common/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Common;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Quantity { get; set; }
+}
